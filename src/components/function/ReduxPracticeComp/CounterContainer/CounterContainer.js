01d10dd51d4d60f10626d9bb29fb5ac545afd913
PoSungKim <psungkr94@likelion.org>
2020-08-10 00:58:05 +0900

    useSelector 최적화: left/right 혹은 shallowEqual을 2번째 Parameter로 사용

--- src/components/function/ReduxPracticeComp/CounterContainer/CounterContainer.js
+++ src/components/function/ReduxPracticeComp/CounterContainer/CounterContainer.js
@@ -1,15 +1,22 @@
 import React from "react";
 import CounterComp from "../CounterComp/CounterComp";
-import {useDispatch, useSelector} from "react-redux";
+import {shallowEqual, useDispatch, useSelector} from "react-redux";
 import {decrease, increase, setDiff} from "../Modules/counter";
 
 function CounterContainer() {
 
     // Container의 역할: rootReducer로 만든 Store 속 state에서 Counter Component에서 필요한 값들을 useSelector로 가져와서 props로 전달해줌
+    {/*
     const {number, diff} = useSelector(state => ({
         number: state.counter.number,
         diff: state.counter.diff,
-    }));
+    }), (left, right) => left.number === right.number && left.diff === right.diff);
+    */}
+
+    const {number, diff} = useSelector(state => ({
+        number: state.counter.number,
+        diff: state.counter.diff,
+    }), shallowEqual);
 
     const dispatch = useDispatch();
     const onIncrease = () => dispatch(increase());
