915f297f90408bdd34985a4db64b6fe3337004ec
PoSungKim <psungkr94@likelion.org>
2020-08-10 20:33:28 +0900

    Redux-thunk 리팩토링: 게시판 개별 페이지의 ID값으로 바로 접근할 수 있도록 객체 형태 개선

--- src/components/function/ReduxThunkComp/Modules/posts.js
+++ src/components/function/ReduxThunkComp/Modules/posts.js
@@ -13,11 +13,44 @@ const CLEAR_POST = 'posts/CLEAR_POST';
 
 const initialState = {
     posts : reducerUtils.initial(),
-    post : reducerUtils.initial()
+    post : {}
 }
 
 const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts', true);
-const getPostReducer = handleAsyncActions(GET_POST, 'post');
+//const getPostReducer = handleAsyncActions(GET_POST, 'post');
+const getPostReducer = (state, action) => {
+    const id = action.meta;
+
+    switch (action.type) {
+        case GET_POST:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    [id] : reducerUtils.loading(state.post[id] && state.post[id].data)
+                }
+            }
+        case GET_POST_SUCCESS:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    [id] : reducerUtils.success(action.payload)
+                }
+            }
+        case GET_POST_ERROR:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    [id] : reducerUtils.error(action.payload)
+                }
+            }
+
+        default :
+            return state;
+    }
+}
 
 export default function posts(state = initialState, action) {
     switch(action.type) {
@@ -41,5 +74,27 @@ export default function posts(state = initialState, action) {
 
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+//export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = (id) => async dispatch => {
+    dispatch({
+        type: GET_POST,
+        meta: id
+    })
+    try {
+        const payload = await postsAPI.getPostById(id);
+        dispatch({
+            type: GET_POST_SUCCESS,
+            payload,
+            meta: id
+        })
+    } catch (error) {
+        dispatch({
+            type: GET_POST_ERROR,
+            payload: error,
+            error: true,
+            meta: id
+        });
+    }
+};
+
 export const clearPost = () => ({type: CLEAR_POST});
--- src/components/function/ReduxThunkComp/PostContainer/PostContainer.js
+++ src/components/function/ReduxThunkComp/PostContainer/PostContainer.js
@@ -1,23 +1,20 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import PostComp from "../PostComp/Postcomp";
-import {getPost, clearPost} from "../Modules/posts";
+import {getPost} from "../Modules/posts";
+import {reducerUtils} from "../lib/asyncUtils";
 
 function PostContainer({postId}) {
 
-    const {data, loading, error} = useSelector(state => state.posts.post);
+    const {data, loading, error} = useSelector(
+        state => state.posts.post[postId] || reducerUtils.initial());
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getPost(postId));
-
-        return () => {
-            dispatch(clearPost());
-        }
-
     }, [postId, dispatch]);
 
-    if (loading)
+    if (loading && !data)
         return <div>로딩 중...</div>;
     if (error)
         return <div>에러 발생! </div>;
