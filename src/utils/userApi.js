4003ca5b978a7db5597dd83b0cce874aa76f4a16
PoSungKim <psungkr94@likelion.org>
2020-08-15 02:03:24 +0900

    Spring Boot Backend Heroku 서버와 React Frontend Github 서버 연동 완료

--- src/utils/userApi.js
+++ src/utils/userApi.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:8080/";
+const DEPLOY_URL = "https://springboot--backend.herokuapp.com/"
 const TEST_URL = "https://jsonplaceholder.typicode.com/users";
 
 export const getUsers = async () => {
     console.log("userApi/getUsers() 함수 실행");
     try {
-        const response = await axios.get(BASE_URL + "users");
+        const response = await axios.get(DEPLOY_URL + "users");
         return response.data;
     } catch (error) {
         console.error(error);
@@ -17,7 +18,7 @@ export const getUsers = async () => {
 export const registerUser = async (action) => {
     console.log("userApi/registerUser() 함수 실행", action);
     try {
-        const response = await axios.post(BASE_URL + "register", action);
+        const response = await axios.post(DEPLOY_URL + "register", action);
         return response.data;
     } catch (error) {
         console.error(error);
@@ -28,7 +29,7 @@ export const registerUser = async (action) => {
 export const findUser = async (action) => {
     console.log("userApi/findUser() 함수 실행", action);
     try {
-        const response = await axios.post(BASE_URL + "finduser", action);
+        const response = await axios.post(DEPLOY_URL + "finduser", action);
         return response.data;
     } catch (error) {
         console.error(error);
@@ -39,7 +40,7 @@ export const findUser = async (action) => {
 export const logInUser = async (action) => {
     console.log("userApi/logIn() 함수 실행", action);
     try {
-        const response = await axios.post(BASE_URL + "login", action);
+        const response = await axios.post(DEPLOY_URL + "login", action);
         return response.data;
     } catch (error) {
         console.error(error);
